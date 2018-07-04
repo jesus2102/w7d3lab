0d93e1dab5292d835ac8253c0bf669d0971a4c59
jesus2102 <jesusperezmarin@gmail.com>
2018-07-04 16:31:10 +0100

    KingArthur created

--- src/main/java/Enemies/KingArthur.java
+++ src/main/java/Enemies/KingArthur.java
@@ -1,4 +1,10 @@
 package Enemies;
 
-public class KingArthur {
+import Heroes.Characters.Character;
+
+public class KingArthur extends Character{
+
+    public KingArthur(String name, int level, int hp, int attack, int defense, int magic){
+        super(name, level, hp,attack,defense,magic);
+    }
 }
