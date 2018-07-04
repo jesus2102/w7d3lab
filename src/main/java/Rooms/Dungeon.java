e6995659ca88f0508fdb035012bd3653e8726864
jesus2102 <jesusperezmarin@gmail.com>
2018-07-04 17:01:51 +0100

    last commit before leaving

--- src/main/java/Rooms/Dungeon.java
+++ src/main/java/Rooms/Dungeon.java
@@ -1,8 +1,16 @@
 package Rooms;
 
+import Enemies.Enemy;
+import Heroes.Characters.Character;
+
 public class Dungeon extends Room {
+
+    private Enemy enemy;
+    private Character hero;
 
     public Dungeon(String name){
         super(name);
+        this.enemy = enemy;
+        this.hero = hero;
     }
 }
--- src/main/java/Rooms/Room.java
+++ src/main/java/Rooms/Room.java
@@ -1,16 +1,13 @@
 package Rooms;
 
-import Heroes.Characters.Character;
-import Enemies.Enemy;
-
 public abstract class Room {
     private String name;
-    private Character hero;
-    private Enemy enemy;
+
 
 
     public Room(String name){
         this.name = name;
+
     }
 
     public String getName() {
