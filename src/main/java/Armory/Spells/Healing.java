92ccc86e6ec87b42fafd1a64b4ec070a802bc974
jesus2102 <jesusperezmarin@gmail.com>
2018-07-04 16:01:40 +0100

    fixing problems

---
+++ src/main/java/Armory/Spells/Healing.java
@@ -0,0 +1,16 @@
+package Armory.Spells;
+
+import Behaviours.ISpell;
+
+public class Healing implements ISpell{
+
+    private int magic;
+
+    public Healing(){
+        this.magic = 50;
+    }
+
+    public int getMagic(){
+        return this.magic;
+    }
+}
