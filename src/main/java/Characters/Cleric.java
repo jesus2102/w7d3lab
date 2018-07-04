4d46845c05698cae8c51fa2676a10e0037d2c457
Chris Craig <ctc2@live.co.uk>
2018-07-04 15:44:41 +0100

    Cleric created along with healing spell

---
+++ src/main/java/Characters/Cleric.java
@@ -0,0 +1,40 @@
+package Characters;
+
+import Behaviours.ISpell;
+
+
+    public class Cleric extends Character {
+
+        ISpell spell;
+
+        public Cleric(String name, int level, int hp, int attack, int defense, int magic, ISpell spell) {
+            super(name, level, hp, attack, defense, magic);
+        }
+
+
+        public int getClericHp() {
+            return getDefaultHp() + (4 * getLevel());
+        }
+
+        public int getClericAttack() {
+            return getDefaultAttack() + (0 * getLevel());
+        }
+
+        public int getClericDefense() {
+            return getDefaultDefense() + (2 * getLevel());
+        }
+
+        public int getClericMagic() {
+            return getDefaultMagic() + (5 * getLevel()) + spell.getMagic();
+        }
+
+        public ISpell getSpell() {
+            return spell;
+        }
+
+        public void setSpell(ISpell spell) {
+            this.spell = spell;
+        }
+
+
+}
